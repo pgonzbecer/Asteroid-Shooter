75c1765ef01a34efe288ec9dedfa04122d41d789
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2016-09-16 20:05:49 -0700

    i did stuf i tink

---
+++ Asteroids Prototype 1 DT/Assets/Rotation_Script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation_Script : MonoBehaviour {
+    public float tumble;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Asteroids Prototype 1 DT/Assets/Scripts/Pauls Scripts/LaserShooter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserShooter : MonoBehaviour {
+	// Variables
+	public GameObject	laser;
+
+	// --- Methods ---
+
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+			spawnLaser();
+	}
+
+	void spawnLaser()
+	{
+		// Variables
+		GameObject	obj=	(GameObject)GameObject.Instantiate(laser);
+	}
+}
